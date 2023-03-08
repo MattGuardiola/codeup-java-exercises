74a1f59f90836badf27d5805f422c53532a592be
Matt Guardiola <matt.g.guardiola@gmail.com>
2023-03-08 14:12:47 -0600

    push

---
+++ src/test/java/CohortTest.java
@@ -0,0 +1,43 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CohortTest {
+
+    private Cohort testCohort;
+    private Student testStudentOne;
+    private Student testStudentTwo;
+
+    @Before
+    public void setUp() {
+        testCohort = new Cohort();
+        testStudentOne = new Student(123456789, "Matt");
+        testStudentTwo = new Student(987654321, "Lauren");
+        testCohort.addStudent(testStudentOne);
+        testCohort.addStudent(testStudentTwo);
+    }
+
+    @Test
+    public void testCohortInstanceCanAddStudentAndGetStudentsList() {
+        Student[] expected = {testStudentOne, testStudentTwo};
+        assertEquals(expected, testCohort.getStudents().toArray(new Student[expected.length]));
+    }
+
+    @Test
+    public void testCohortInstanceCanGetGradesAverage() {
+        testStudentOne.addGrade(100);
+        testStudentTwo.addGrade(50);
+        assertEquals(75, testCohort.getCohortAverage(), 0);
+    }
+
+    @Test
+    public void testFindAStudentMethodWithID() {
+        assertEquals(123456789, testStudentOne.getId());
+    }
+
+}
+
+
+
+
